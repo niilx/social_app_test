ecd35461ed378ad249bd9a6d317f87eec5ae6cde
niilx <Hridoy82>
2025-01-30 10:25:40 +0600

    newsfeed all features done including pagination and custom bubble reaction

--- lib/views/newsfeed/newsfeedScreen.dart
+++ lib/views/newsfeed/newsfeedScreen.dart
@@ -1,10 +1,20 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:social_test/controllers/newsfeed/newsfeedController.dart';
+import 'package:social_test/helpers/dialogs.dart';
+import 'package:social_test/helpers/images.dart';
 import 'package:social_test/helpers/projectResources.dart';
 import 'package:social_test/helpers/routes.dart';
+import 'package:social_test/helpers/texts.dart';
+import 'package:social_test/models/communityListModel.dart';
 import 'package:social_test/styles/colors.dart';
+import 'package:social_test/views/newsfeed/commentScreen.dart';
 import 'package:social_test/views/newsfeed/createPostScreen.dart';
+
+import '../../controllers/authentication/loginController.dart';
+import 'postWidgetScreen.dart';
 
 class NewsfeedScreen extends StatefulWidget {
   const NewsfeedScreen({Key? key}) : super(key: key);
@@ -34,6 +44,7 @@ class _NewsfeedScreenState extends State<NewsfeedScreen> {
           }
         }
       });
+
     });
   }
 
@@ -106,15 +117,14 @@ class _NewsfeedScreenState extends State<NewsfeedScreen> {
 
   getNewsfeedList(){
     return ListView.builder(
-
         shrinkWrap: true,
         itemCount: newsfeedControllerVar.newsFeedList.length,
         physics: const NeverScrollableScrollPhysics(),
         itemBuilder: (BuildContext context, int index) {
-          return PostWidget(username: newsfeedControllerVar.newsFeedList[index].user?.fullName??'',
-            timeAgo: newsfeedControllerVar.newsFeedList[index].createdAt.toString(),
-            content: newsfeedControllerVar.newsFeedList[index].feedTxt??'',
-            screenWidth: ProjectResource.screenWidth * .9,);
+          return PostWidget(
+            newsfeedData: newsfeedControllerVar.newsFeedList[index],
+            index: index,
+          );
         });
   }
 
@@ -142,26 +152,47 @@ class _NewsfeedScreenState extends State<NewsfeedScreen> {
         backgroundColor: AppColors.themeColor,
       ),
       drawer: Drawer(),
-      body: SingleChildScrollView(
-        controller: scrollController,
-        child: Column(
-          children: [
-            // Post input
-            getPostButton(),
-            newsfeedControllerVar.loading
-                ? Center(
-              child: CircularProgressIndicator(),
-            )
-                : newsfeedControllerVar.loading
-                ? ProjectResource.getErrorView()
-                : getNewsfeedList(),
-            newsfeedControllerVar.loadingMore
-                ? Center(
-                child: CircularProgressIndicator()):Container(),
-          ],
+      body: PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, Object? result)  async {
+        ProjectResource.onWillPopExit(context: context);
+      },
+        child: GestureDetector(
+          onTap: (){
+            if(newsfeedControllerVar.showReactions) {
+              newsfeedController.removePopup();
+            }
+          },
+          child: SingleChildScrollView(
+            controller: scrollController,
+            child: Column(
+              children: [
+                // Post input
+                getPostButton(),
+                newsfeedControllerVar.loading
+                    ? Center(
+                  child: CircularProgressIndicator(),
+                )
+                    : newsfeedControllerVar.loading
+                    ? ProjectResource.getErrorView()
+                    : getNewsfeedList(),
+                newsfeedControllerVar.loadingMore
+                    ? Center(
+                    child: CircularProgressIndicator()):Container(),
+              ],
+            ),
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: (index){
+          if(index == 1){
+            Dialogs.logoutDialog(context, onResponse: (){
+              Navigator.pop(context);
+              Provider.of<LoginController>(context,listen: false).performLogout(context: context);
+            }, title: "Logout", content: "Are you sure you want to logout?", buttonText: "Yes");
+          }
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.people),
@@ -175,194 +206,4 @@ class _NewsfeedScreenState extends State<NewsfeedScreen> {
       ),
     );
   }
-}
-
-class PostWidget extends StatefulWidget {
-  final String username;
-  final String timeAgo;
-  final String content;
-  final String? imageUrl;
-  final double screenWidth;
-
-  const PostWidget({
-    Key? key,
-    required this.username,
-    required this.timeAgo,
-    required this.content,
-    this.imageUrl,
-    required this.screenWidth,
-  }) : super(key: key);
-
-  @override
-  _PostWidgetState createState() => _PostWidgetState();
-}
-
-class _PostWidgetState extends State<PostWidget> with TickerProviderStateMixin {
-  bool showReactions = false;
-  late AnimationController _animationController;
-  late Animation<double> _opacityAnimation;
-  late Animation<Offset> _positionAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 200),
-      vsync: this,
-    );
-
-    _opacityAnimation = Tween<double>(begin: 0, end: 1).animate(_animationController);
-    _positionAnimation = Tween<Offset>(begin: const Offset(0, 0.3), end: const Offset(0, 0))
-        .animate(_animationController);
-  }
-
-  void toggleReactions() {
-    if (showReactions) {
-      _animationController.reverse();
-        setState(() => showReactions = false);
-
-    } else {
-      setState(() => showReactions = true);
-      _animationController.forward();
-    }
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Header
-            Row(
-              children: [
-                const CircleAvatar(
-                  child: Icon(Icons.person),
-                ),
-                const SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.username,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(widget.timeAgo),
-                  ],
-                ),
-                const Spacer(),
-                IconButton(
-                  icon: const Icon(Icons.more_vert),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            // Content
-            Text(widget.content),
-            const SizedBox(height: 10),
-            if (widget.imageUrl != null)
-              Image.network(
-                widget.imageUrl!,
-                width: widget.screenWidth,
-                fit: BoxFit.cover,
-              ),
-            const SizedBox(height: 10),
-            // Reactions
-            Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Row(
-                  children: [
-                    GestureDetector(
-                      onLongPressDown: (_) {
-                        toggleReactions();
-                      },
-                      onTap: () {
-                        // Handle like
-                      },
-                      child: const Text('👍 Like'),
-                    ),
-                    Spacer(),
-                    const Text('💬 Comment'),
-                  ],
-                ),
-                if (showReactions)
-                  Positioned(
-                    bottom: 30,
-                    child: SlideTransition(
-                      position: _positionAnimation,
-                      child: FadeTransition(
-                        opacity: _opacityAnimation,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(30),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.2),
-                                blurRadius: 5,
-                              ),
-                            ],
-                          ),
-                          child: Row(
-                            children: const [
-                              ReactionBubble(reaction: '👍'),
-                              ReactionBubble(reaction: '❤'),
-                              ReactionBubble(reaction: '😢'),
-                              ReactionBubble(reaction: '😘'),
-                              ReactionBubble(reaction: '😡'),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ReactionBubble extends StatelessWidget {
-  final String reaction;
-
-  const ReactionBubble({Key? key, required this.reaction}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.symmetric(horizontal: 4),
-        padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              blurRadius: 4,
-            ),
-          ],
-        ),
-        child: Text(
-            reaction,
-            style: const TextStyle(fontSize: 20),
-            ),
-        );
-    }
 }
